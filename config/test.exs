16fdb281fc275d7cbfade370561dcea7c20d62e0
Kairen1997 <kairen.deiwien@gmail.com>
2025-07-03 10:20:45 +0800

    all finance-app code

---
+++ config/test.exs
@@ -0,0 +1,37 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :finance_app, FinanceApp.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "finance_app_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: System.schedulers_online() * 2
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :finance_app, FinanceAppWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "gc8h2GevaX6vyQ89WShREmu0y2eMyCpV3cmPhg9Gz5EHrI/A90cRRXQmeqxHLbNq",
+  server: false
+
+# In test we don't send emails
+config :finance_app, FinanceApp.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
+
+# Enable helpful, but potentially expensive runtime checks
+config :phoenix_live_view,
+  enable_expensive_runtime_checks: true
---
+++ lib/finance_app/mailer.ex
@@ -0,0 +1,3 @@
+defmodule FinanceApp.Mailer do
+  use Swoosh.Mailer, otp_app: :finance_app
+end
